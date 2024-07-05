d66807a26263064bcf547eaeb016e8f1e59f2265
Zahrara02 <zahradepok291@gmail.com>
2024-07-05 19:50:21 +0700

    fead(menghapus sign up dan mengubah kata paling bawah)hapus dan ubah

--- resources/views/user/layouts/footer.blade.php
+++ resources/views/user/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="footer_section">
     <div class="container">
-        <p>&copy; <span id="displayYear"></span> All Rights Reserved By <a href="https://html.design/">Free Html Templates</a> Distribution <a href="https://themewagon.com">ThemeWagon</a></p>
+        <p>&copy; <span id="displayYear"></span> Pengajuan Cuti Pegawai</p>
     </div>
 </footer>
